a4842a0e6459e50c9a2d68f9bc6356527cfa5e38
Raphael Marques <raphaeljcmarques@gmail.com>
2022-05-21 18:59:27 -0300

    REFACTOR: Modal Component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,34 +1,17 @@
-import { Component } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: JSX.Element;
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
-      <ReactModal
+const Modal = ({ isOpen, setIsOpen, children }: ModalProps) => {
+  return (
+    <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
-        isOpen={modalStatus}
+        isOpen={isOpen}
         ariaHideApp={false}
         style={{
           content: {
@@ -50,9 +33,8 @@ class Modal extends Component {
         }}
       >
         {children}
-      </ReactModal>
-    );
-  }
-};
+    </ReactModal>
+  );
+}
 
 export default Modal;
